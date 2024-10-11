52d737997091cf9835f3e0fc189b61afa292a0ed
andrewwillette <willette.andrew1846@gmail.com>
2024-10-10 22:09:56 -0500

    building out keyofday

--- lua/willette-scripts/keyofday.lua
+++ lua/willette-scripts/keyofday.lua
@@ -1,38 +1,41 @@
 local M = {}
 
+---@class KeyOfDay
+---@field key string
+---@field date os.time
+
+local keys = {
+  "A",
+  "A#",
+  "B",
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+}
+
+local sept26th2024 = {
+  key = keys[0],
+  date = os.time({ year = 2024, month = 9, day = 26 }),
+}
+
 function days_difference(epoch1, epoch2)
-  -- Calculate the difference in seconds
   local diff_in_seconds = os.difftime(epoch2, epoch1)
-
-  -- Convert seconds to days (1 day = 86400 seconds)
-  local diff_in_days = diff_in_seconds / (60 * 60 * 24)
-
+  local diff_in_days = math.floor(diff_in_seconds / (60 * 60 * 24))
   return diff_in_days
 end
 
-M.keyofday = function()
-  --- Oct 8th is Key of A
-  --- each day goes up chromatically a half step
-  local keys = {
-    "A",
-    "A#",
-    "B",
-    "C",
-    "C#",
-    "D",
-    "D#",
-    "E",
-    "F",
-    "F#",
-    "G",
-    "G#",
-  }
-  -- print("day of oct8 is " .. os.date("%d", oct8))
-  local oct8 = os.date("*t", os.time({ year = 2024, month = 10, day = 8 }))
-  local today = os.date("*t")
-  print(today.day)
-  -- print(oct8.day)
-  local key = keys[today.day % 12 + 1]
+M.keyofday = function(date)
+  local daysdifference = days_difference(sept26th2024.date, date)
+  local key = daysdifference % 12
+  print("key number is " .. key)
+  print("key of day is " .. keys[key])
+  print("days difference is " .. daysdifference)
   return key
 end
 
