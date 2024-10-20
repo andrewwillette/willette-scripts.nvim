874df5f7a66329a6fef77d948ec2b018435d4c6a
andrewwillette <willette.andrew1846@gmail.com>
2024-10-20 18:53:46 -0500

    removing keyofday after refactoring into separate plugin

--- lua/willette-scripts/keyofday_spec.lua
+++
@@ -1,8 +0,0 @@
-local keyofday = require("willette-scripts.keyofday")
-
-describe("keyofday", function()
-  it("should return the key of the day", function()
-    local key = keyofday.keyofday()
-    vim.print(key)
-  end)
-end)
